7a2f01e74ec09cd82776ce8193f5342701198b4d
Michael Duren <michaeld@michaelduren.com>
2023-06-04 13:17:39 -0500

    moved services to extension method from program.cs

---
+++ Extensions/AppServicesExtensions.cs
@@ -0,0 +1,80 @@
+using CustomerFeedback.Context;
+using CustomerFeedback.Models;
+using FluentValidation;
+using FluentValidation.AspNetCore;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
+namespace CustomerFeedback.Extensions
+{
+    public static class AppServicesExtensions
+    {
+        public static IServiceCollection AddAppServices(
+            this IServiceCollection services,
+            IConfiguration config,
+            string connection
+        )
+        {
+            // Add services to the container.
+            services.AddControllersWithViews();
+            services.AddEndpointsApiExplorer();
+
+            // add swagger
+            services.AddSwaggerGen(x =>
+            {
+                x.AddSecurityDefinition(
+                    "Bearer",
+                    new OpenApiSecurityScheme
+                    {
+                        Description = "JWT Authorization header using the Bearer scheme.",
+                        Name = "Authorization",
+                        In = ParameterLocation.Header,
+                        Type = SecuritySchemeType.ApiKey,
+                    }
+                );
+                x.AddSecurityRequirement(
+                    new OpenApiSecurityRequirement
+                    {
+                        {
+                            new OpenApiSecurityScheme
+                            {
+                                Reference = new OpenApiReference
+                                {
+                                    Type = ReferenceType.SecurityScheme,
+                                    Id = "Bearer"
+                                }
+                            },
+                            new List<string>()
+                        }
+                    }
+                );
+            });
+
+            // add fluent validation
+            services.AddFluentValidationAutoValidation();
+            services.AddFluentValidationClientsideAdapters();
+            services.AddValidatorsFromAssemblyContaining<Feedback>();
+
+            // add Authentication with JWT
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
+
+            // add authorization policy
+            services.AddAuthorization(options =>
+            {
+                options.FallbackPolicy = new AuthorizationPolicyBuilder()
+                    .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
+                    .RequireAuthenticatedUser()
+                    .Build();
+            });
+
+            services.AddDbContext<AppDbContext>(
+                options =>
+                    options.UseSqlServer(connection, options => options.EnableRetryOnFailure())
+            );
+
+            return services;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,82 +1,20 @@
 using CustomerFeedback;
 using CustomerFeedback.Context;
 using CustomerFeedback.EndpointDefinitions;
-using CustomerFeedback.Models;
-using FluentValidation;
-using FluentValidation.AspNetCore;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.Authorization;
+using CustomerFeedback.Extensions;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
-
-// Add services to the container.
-builder.Services.AddControllersWithViews();
-builder.Services.AddEndpointsApiExplorer();
-
-// add swagger
-builder.Services.AddSwaggerGen(x =>
-{
-    x.AddSecurityDefinition(
-        "Bearer",
-        new OpenApiSecurityScheme
-        {
-            Description = "JWT Authorization header using the Bearer scheme.",
-            Name = "Authorization",
-            In = ParameterLocation.Header,
-            Type = SecuritySchemeType.ApiKey,
-        }
-    );
-    x.AddSecurityRequirement(
-        new OpenApiSecurityRequirement
-        {
-            {
-                new OpenApiSecurityScheme
-                {
-                    Reference = new OpenApiReference
-                    {
-                        Type = ReferenceType.SecurityScheme,
-                        Id = "Bearer"
-                    }
-                },
-                new List<string>()
-            }
-        }
-    );
-});
-
-// add fluent validation
-builder.Services.AddFluentValidationAutoValidation();
-builder.Services.AddFluentValidationClientsideAdapters();
-builder.Services.AddValidatorsFromAssemblyContaining<Feedback>();
-
-// add Authentication with JWT
-builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer();
-
-// add authorization policy
-builder.Services.AddAuthorization(options =>
-{
-    options.FallbackPolicy = new AuthorizationPolicyBuilder()
-        .AddAuthenticationSchemes(JwtBearerDefaults.AuthenticationScheme)
-        .RequireAuthenticatedUser()
-        .Build();
-});
 
 // get connection string dependent on environment
 var connection = String.Empty;
 if (builder.Environment.IsDevelopment())
-{
     connection = builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING");
-}
 else
-{
     connection = Environment.GetEnvironmentVariable("AZURE_SQL_CONNECTIONSTRING");
-}
 
-builder.Services.AddDbContext<AppDbContext>(
-    options => options.UseSqlServer(connection, options => options.EnableRetryOnFailure())
-);
+// add services
+builder.Services.AddAppServices(builder.Configuration, connection!);
 
 // build app
 var app = builder.Build();
@@ -98,13 +36,7 @@ app.UseRouting();
 app.UseAuthentication();
 app.UseAuthorization();
 
-// don't map controllers
-// app.MapControllerRoute(name: "default", pattern: "{controller}/{action=Index}/{id?}");
-
 app.MapFallbackToFile("index.html");
-
-// api endpoints
-app.MapGet("/api/hello", () => "Hello World!");
 
 // feedback endpoitns
 FeedbackEndpoint.Map(app);
