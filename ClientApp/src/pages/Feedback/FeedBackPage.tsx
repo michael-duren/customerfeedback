be24dbf450910323b81c250ade64fb731bdaa158
Michael Duren <michaeld@michaelduren.com>
2023-06-04 11:59:31 -0500

    added spinner to fback comp

--- ClientApp/src/pages/Feedback/FeedBackPage.tsx
+++ ClientApp/src/pages/Feedback/FeedBackPage.tsx
@@ -5,11 +5,13 @@ import './FeedbackPage.styles.css';
 import { AiFillStar } from 'react-icons/ai';
 import dayjs from 'dayjs';
 import realtiveTime from 'dayjs/plugin/relativeTime';
+import { Spinner } from 'reactstrap';
 
 dayjs.extend(realtiveTime);
 
 export default function FeedbackPage() {
   const [feedback, setFeedback] = useState<Feedback[]>([]);
+  const [isLoading, SetIsLoading] = useState<boolean>(true);
 
   const getFeedback = async () => {
     const feedback = axios.get('/api/feedback');
@@ -17,7 +19,10 @@ export default function FeedbackPage() {
   };
 
   useEffect(() => {
-    getFeedback().then((data) => setFeedback(data));
+    getFeedback().then((data) => {
+      setFeedback(data);
+      SetIsLoading(false);
+    });
   }, []);
 
   console.log(feedback);
@@ -26,33 +31,43 @@ export default function FeedbackPage() {
     <main className="feedback-page">
       <h1 className="h2">Feedback</h1>
       <div>
-        <ul className="feedback-list">
-          {feedback.map((feedback) => {
-            return (
-              <li
-                className={`feedback-card ${
-                  feedback.rating > 3
-                    ? 'positive'
-                    : feedback.rating === 3
-                    ? 'medium'
-                    : 'negative'
-                }`}
-                key={feedback.id}
-              >
-                <div className="star">
-                  {Array.from({ length: feedback.rating }).map((_, i) => {
-                    return <AiFillStar key={i} size={35} />;
-                  })}
-                </div>
-                <h3 className="h4">{feedback.title}</h3>
-                <p>{feedback.description}</p>
-                <div className="time">
-                  {dayjs(feedback.dateReviewed).fromNow()}
-                </div>
-              </li>
-            );
-          })}
-        </ul>
+        {isLoading ? (
+          <Spinner
+            className="m-5"
+            color="primary"
+            style={{ height: '3rem', width: '3rem' }}
+          >
+            Loading Reviews...
+          </Spinner>
+        ) : (
+          <ul className="feedback-list">
+            {feedback.map((feedback) => {
+              return (
+                <li
+                  className={`feedback-card ${
+                    feedback.rating > 3
+                      ? 'positive'
+                      : feedback.rating === 3
+                      ? 'medium'
+                      : 'negative'
+                  }`}
+                  key={feedback.id}
+                >
+                  <div className="star">
+                    {Array.from({ length: feedback.rating }).map((_, i) => {
+                      return <AiFillStar key={i} size={35} />;
+                    })}
+                  </div>
+                  <h3 className="h4">{feedback.title}</h3>
+                  <p>{feedback.description}</p>
+                  <div className="time">
+                    {dayjs(feedback.dateReviewed).fromNow()}
+                  </div>
+                </li>
+              );
+            })}
+          </ul>
+        )}
       </div>
     </main>
   );
