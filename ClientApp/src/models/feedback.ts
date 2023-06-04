7f67291381678488016867313713991536dda9f9
Michael Duren <michaeld@michaelduren.com>
2023-06-04 10:50:42 -0500

    added feedback list and useEffect to feedback page

---
+++ ClientApp/src/models/feedback.ts
@@ -0,0 +1,7 @@
+export interface Feedback {
+  id: number;
+  title: string;
+  description: string;
+  rating: number;
+  dateReviewed: string;
+}
--- ClientApp/src/pages/Feedback/FeedBackPage.tsx
+++ ClientApp/src/pages/Feedback/FeedBackPage.tsx
@@ -1,7 +1,37 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+import { Feedback } from '../../models/feedback';
+import './FeedbackPage.styles.css';
+
 export default function FeedbackPage() {
+  const [feedback, setFeedback] = useState<Feedback[]>([]);
+
+  const getFeedback = async () => {
+    const feedback = axios.get('/api/feedback');
+    return (await feedback).data;
+  };
+
+  useEffect(() => {
+    getFeedback().then((data) => setFeedback(data));
+  }, []);
+
+  console.log(feedback);
+
   return (
-    <div>
-      <h1>Feedback</h1>
-    </div>
+    <main className="feedback-page">
+      <h1 className="h2">Feedback</h1>
+      <div>
+        <ul className="feedback-list">
+          {feedback.map((feedback) => {
+            return (
+              <li className="feedback-card" key={feedback.id}>
+                <h3 className="h4">{feedback.title}</h3>
+                <p>{feedback.description}</p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </main>
   );
 }
