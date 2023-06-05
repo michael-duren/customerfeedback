4974afc1ca0d47cb64e0ccdc8454891d98569fad
Michael Duren <michaeld@michaelduren.com>
2023-06-05 04:44:21 -0500

    add user DTO

---
+++ Models/DTOs/AppUserDto.cs
@@ -0,0 +1,8 @@
+namespace CustomerFeedback.Models.DTOs
+{
+    public class AppUserDto
+    {
+        public string Email { get; set; } = null!;
+        public string Token { get; set; } = null!;
+    }
+}
