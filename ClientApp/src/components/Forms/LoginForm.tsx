4dfe50c8218390f85fef46aa8414d548b1e68f27
Michael Duren <michaeld@michaelduren.com>
2023-06-12 10:47:28 -0500

    validation added for login

--- ClientApp/src/components/Forms/LoginForm.tsx
+++ ClientApp/src/components/Forms/LoginForm.tsx
@@ -11,10 +11,11 @@ import {
   Spinner,
   Alert,
 } from 'reactstrap';
-import { Formik } from 'formik';
+import { useFormik } from 'formik';
 import { useStore } from '../../stores/store';
 import { useEffect } from 'react';
 import { observer } from 'mobx-react-lite';
+import { loginFormSchema } from './schemas/loginFormSchema';
 
 interface Props {
   modal: boolean;
@@ -36,66 +37,75 @@ export default observer(function LoginForm({
     }
   }, [user]);
 
+  const formik = useFormik({
+    initialValues: { email: '', password: '', errors: null },
+    validationSchema: loginFormSchema,
+    onSubmit: async (values) => {
+      try {
+        await login(values);
+      } catch (e) {
+        formik.setErrors({ errors: 'Invalid email or password' });
+      }
+    },
+  });
+
   return (
     <Modal isOpen={modal} toggle={toggle}>
       <ModalHeader toggle={toggle}>Welcome Back! Login Below!</ModalHeader>
-      <Formik
-        initialValues={{ email: '', password: '', errors: null }}
-        onSubmit={async (values, { setErrors }) => {
-          try {
-            await login(values);
-          } catch (e) {
-            setErrors({ errors: 'Invalid email or password' });
-          }
-        }}
-      >
-        {({ values, errors, handleChange, handleSubmit, isSubmitting }) => (
-          <Form onSubmit={handleSubmit}>
-            <ModalBody>
-              <FormGroup>
-                <Label for="email">Email</Label>
-                <Input
-                  onChange={handleChange}
-                  value={values.email}
-                  id="email"
-                  name="email"
-                  type="email"
-                  required
-                />
-              </FormGroup>
-              <FormGroup>
-                <Label for="password">Password</Label>
-                <Input
-                  id="password"
-                  onChange={handleChange}
-                  value={values.password}
-                  name="password"
-                  type="password"
-                  required
-                />
-                {errors.errors && (
-                  <Alert className="mt-2 p-2" color="danger">
-                    {errors.errors}
-                  </Alert>
-                )}
-              </FormGroup>
-            </ModalBody>
-            <ModalFooter>
-              <Button
-                type="submit"
-                disabled={isSubmitting}
-                color="success"
-                outline
-              >
-                {isSubmitting ? <Spinner size={'sm'} /> : <span>Login</span>}
-              </Button>{' '}
-              <Button color="danger" outline onClick={toggle}>
-                Cancel
-              </Button>
-            </ModalFooter>
-          </Form>
-        )}
-      </Formik>
+      <Form onSubmit={formik.handleSubmit}>
+        <ModalBody>
+          <FormGroup>
+            <Label for="email">Email</Label>
+            <Input
+              onChange={formik.handleChange}
+              value={formik.values.email}
+              id="email"
+              name="email"
+              type="email"
+              required
+            />
+            {formik.touched.email && formik.errors.email ? (
+              <Alert className="mt-2 p-2" color="danger">
+                {formik.errors.email}
+              </Alert>
+            ) : null}
+          </FormGroup>
+          <FormGroup>
+            <Label for="password">Password</Label>
+            <Input
+              id="password"
+              onChange={formik.handleChange}
+              value={formik.values.password}
+              name="password"
+              type="password"
+              required
+            />
+            {formik.touched.password && formik.errors.password ? (
+              <Alert className="mt-2 p-2" color="danger">
+                {formik.errors.password}
+              </Alert>
+            ) : null}
+            {formik.errors.errors && (
+              <Alert className="mt-2 p-2" color="danger">
+                {formik.errors.errors}
+              </Alert>
+            )}
+          </FormGroup>
+        </ModalBody>
+        <ModalFooter>
+          <Button
+            type="submit"
+            disabled={formik.isSubmitting}
+            color="success"
+            outline
+          >
+            {formik.isSubmitting ? <Spinner size={'sm'} /> : <span>Login</span>}
+          </Button>{' '}
+          <Button color="danger" outline onClick={toggle}>
+            Cancel
+          </Button>
+        </ModalFooter>
+      </Form>
     </Modal>
   );
 });
